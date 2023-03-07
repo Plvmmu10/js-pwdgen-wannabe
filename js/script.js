3015b222147512f9f7cd352d0be247eb976ec022
Plvmmu10 <gluca0260@gmail.com>
2023-03-07 16:05:23 +0100

    fix grammar errors

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-let nome = prompt ("Inserisci il tuo nome");
+let userName = prompt ("Inserisci il tuo nome");
 let surname = prompt ("Inserisci il tuo cognome");
 let color = prompt ("Inserisci il tuo colore preferito");
-const pswNumber = 22
+const pswNumber = 22;
 
-console.log(nome, surname, color, pswNumber)
+console.log(userName, surname, color, pswNumber);
 
 let password = document.getElementById("psw").innerHTML = 
-`<h1>${nome}${surname}${color}${pswNumber}</h1>`
-;
+`<h1>${userName}${surname}${color}${pswNumber}</h1>`;
+
