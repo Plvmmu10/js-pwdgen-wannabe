d85d9b0e6fad7ea73ebbe3895c887eaed8905a1c
Plvmmu10 <gluca0260@gmail.com>
2023-03-07 16:13:27 +0100

    fixed spacing in all prompts

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-let userName = prompt ("Inserisci il tuo nome");
-let surname = prompt ("Inserisci il tuo cognome");
-let color = prompt ("Inserisci il tuo colore preferito");
+let userName = prompt("Inserisci il tuo nome");
+let surname = prompt("Inserisci il tuo cognome");
+let color = prompt("Inserisci il tuo colore preferito");
 const pswNumber = 22;
 
 console.log(userName, surname, color, pswNumber);
